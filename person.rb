d9df677fc6384d58a580e1e088e9b8b0b943c58f
lenkon <inoulenkon@gmail.com>
2023-04-19 13:48:01 +0530

    Add decorators and make Person subclass of Nameable

--- person.rb
+++ person.rb
@@ -1,5 +1,8 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -15,7 +18,13 @@ class Person
     @age >= 18
   end
 
+  public
+
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
