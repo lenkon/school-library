c6fbc2822d4aee68754d2c995be8b0f60b43a358
lenkon <inoulenkon@gmail.com>
2023-04-21 21:28:08 +0530

    Add create book method

--- app.rb
+++ app.rb
@@ -124,5 +124,15 @@ class App
     when 2
       create_teacher
     end
-  end  
+  end
+
+  def create_book
+    puts
+    print 'Title: '
+    title = gets.chomp.strip.capitalize
+    print 'Author: '
+    author = gets.chomp.strip.capitalize
+    @books << Book.new(title, author)
+    puts 'Book created successfully'
+  end
 end
